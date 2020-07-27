f7cfec8050a40c529ea546990373a966bc62659e
Rodrigo Martins <rodrigomartins_santos1804@hotmail.com>
2020-07-27 18:30:21 -0300

    Standard api response

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,20 +3,20 @@ const {Account} = require('../models')
 const {hashSync} = require('bcrypt')
 
 const router = express.Router();
-const SaltRounds = 10;
 
 router.get('/sign-in', async (req, res) => {
     
     const {email , password} = req.body;
 
+
     const EmailExists = await Account.findOne({where : {email}});
     
-    if(EmailExists) return res.json('Account already exists');
+    if(EmailExists) return res.jsonBadRequest(null, 'Account already exists');
     
     const hashPassword = hashSync(password, 10)
     const NewAccount = await Account.create({email , password: hashPassword})
     
-    return res.json(NewAccount)
+    return res.jsonOK(NewAccount, "Account created")
 }) 
 
 module.exports = router
---
+++ src/middlewares/response.js
@@ -0,0 +1,69 @@
+const TYPE_JSON = 'application/json'
+const STATUS_CODE_OK = 200
+const STATUS_CODE_BAD_REQUEST = 400
+const STATUS_CODE_UNAUTHORIZED = 401
+const STATUS_CODE_NOT_FOUND = 404
+const STATUS_CODE_SERVER_ERROR = 500
+
+const jsonOK = function(data, message, metadata) {
+
+    message = (message) ? message : 'Successful request';
+    metadata = (metadata) ? metadata : {} ;
+
+    this.status(STATUS_CODE_OK)
+    this.type(TYPE_JSON);
+    return this.json ({message, data , metadata, status : STATUS_CODE_OK})
+}
+
+const jsonBadRequest= function(data, message, metadata) {
+
+    message = (message) ? message : 'Bad request';
+    metadata = (metadata) ? metadata : {} ;
+
+    this.status(STATUS_CODE_BAD_REQUEST)
+    this.type(TYPE_JSON);
+    return this.json ({message, data , metadata, status : STATUS_CODE_BAD_REQUEST})
+}
+
+const jsonUnauthorized = function(data, message, metadata) {
+
+    message = (message) ? message : 'Unauthorized request';
+    metadata = (metadata) ? metadata : {} ;
+
+    this.status(STATUS_CODE_UNAUTHORIZED)
+    this.type(TYPE_JSON);
+    return this.json ({message, data , metadata, status : STATUS_CODE_UNAUTHORIZED})
+}
+
+const jsonNotFound = function(data, message, metadata) {
+
+    message = (message) ? message : 'Not Found ';
+    metadata = (metadata) ? metadata : {} ;
+
+    this.status(STATUS_CODE_NOT_FOUND)
+    this.type(TYPE_JSON);
+    return this.json ({message, data , metadata, status : STATUS_CODE_NOT_FOUND})
+}
+
+const jsonServerError = function(data, message, metadata) {
+
+    message = (message) ? message : 'Server Error';
+    metadata = (metadata) ? metadata : {} ;
+
+    this.status(STATUS_CODE_SERVER_ERROR)
+    this.type(TYPE_JSON);
+    return this.json ({message, data , metadata, status : STATUS_CODE_SERVER_ERROR})
+}
+
+const response = (req,res, next) => {
+
+    res.jsonOK = jsonOK;
+    res.jsonBadRequest = jsonBadRequest;
+    res.jsonUnauthorized = jsonUnauthorized;
+    res.jsonNotFound = jsonNotFound;
+    res.jsonNotFound = jsonServerError;
+
+    next();
+}
+
+module.exports = response;
