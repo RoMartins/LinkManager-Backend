f900c0298a131cb338ad5aea263fdb03f1c701f1
Rodrigo Martins <rodrigomartins_santos1804@hotmail.com>
2020-07-28 20:12:23 -0300

    Links CRUD

---
+++ src/controllers/link.js
@@ -0,0 +1,74 @@
+ const express = require('express');
+
+const router = express.Router();
+
+const {Link} = require('../models')
+
+router.get('/' , async (req, res)=> {
+    const accountId = 1
+
+    let links = await Link.findAll({where : {accountId}})
+    if(!links) return res.jsonNotFound();
+
+    return res.jsonOK(links)
+})
+
+router.get('/:id' , async (req, res)=> {
+    const {id} = req.params
+    const accountId = 1
+
+    let link = await Link.findOne({where : {id, accountId}})
+    if(!link) return res.jsonNotFound();
+
+    return res.jsonOK(link)
+})
+
+router.post('/' , async (req, res)=> {
+
+    const {label, url, image, isSocial} = req.body;
+
+    const accountId = 1
+
+    const link = await  Link.create({
+        label,
+        isSocial,
+        image,
+        url,
+        accountId
+    })
+
+    return res.jsonOK(link)
+})
+
+router.put('/:id' , async (req, res) => {
+    const {id} = req.params
+    
+    const accountId = 1
+    const fields = ['label' , 'url', 'isSocial', 'image']
+
+    let link = await Link.findOne({where : {id, accountId}})
+    if(!link) return res.jsonNotFound();
+
+    fields.map(fieldName => {
+        const newValue = req.body[fieldName]
+        if(newValue) link[fieldName] = newValue
+    })
+
+    await link.save();
+
+    return res.jsonOK(link)
+   
+})
+
+router.delete('/:id',  async (req, res) => {
+    const {id} = req.params
+    const accountId = 1
+
+    let link = await Link.findOne({where : {id, accountId}})
+    if(!link) return res.jsonNotFound();
+
+     link.destroy()
+
+    return res.jsonOK()
+})
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const response = require('./middlewares/response')
 
 const db = require('./models')
 const authController = require("./controllers/auth")
+const linkController = require("./controllers/link")
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
 app.use('/auth', authController)
+app.use('/link', linkController)
 
 db.sequelize.sync().then(() => {
     app.listen(3003 , () => {
